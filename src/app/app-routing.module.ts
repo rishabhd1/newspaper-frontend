4752a5803d9c599fb9ffc2e4ab8b941d8bfbdab8
Rishabh Dubey <rishabhdubey020@gmail.com>
2020-01-08 00:32:42 +0530

    added headlines component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HeadlinesComponent } from './components/headlines/headlines.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HeadlinesComponent },
+  { path: 'headlines', component: HeadlinesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MaterialModule } from './material/material.module';
+import { HeadlinesComponent } from './components/headlines/headlines.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeadlinesComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
